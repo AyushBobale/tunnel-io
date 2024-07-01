930fa52b822ce897b35ef52ae6f42954f387cc0c
Ayush Bobale <bobaleayush@gmail.com>
2024-07-01 21:29:03 +0530

    Change Log
    1. Added roll up for bundling
    2. added cypress for tests
    3. inital run test performed
    4. TODO : cypress linting vs code

---
+++ rollup.config.mjs
@@ -0,0 +1,19 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+
+export default {
+  input: "dist/index.js", // Your entry point
+  output: {
+    file: "dist/bundle.js",
+    format: "umd", // Universal Module Definition, works as amd, cjs and iife all in one
+    name: "TunnelIO", // The global variable name representing your library
+  },
+  plugins: [
+    resolve(), // Helps Rollup find modules in node_modules
+    commonjs(), // Converts CommonJS modules to ES6
+    // typescript(), // Converts TypeScript to JavaScript
+    terser(), // Minifies the bundle
+  ],
+};
